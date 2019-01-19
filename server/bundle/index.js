3dd5bde0bc1cd953a7cee585ae26a8c7e1e44b0e
adamkcolton <adamcolton216@gmail.com>
2019-01-19 13:23:27 -0800

    updated molecules and elements api gateway for dynamo

--- server/bundle/index.js
+++ server/bundle/index.js
@@ -1,15 +1,48 @@
 'use strict';
 
-const { Webhook, ExpressJS, Lambda } = require('jovo-framework');
-const { alexa } = require('./alexa.js');
+const {
+  Webhook,
+  ExpressJS,
+  Lambda
+} = require('jovo-framework');
+const {
+  alexa
+} = require('./alexa.js');
 const DB = require('./dynamo');
 const Dynamo = new DB();
 
+const done = response => {
+  return {
+    statusCode: '200',
+    body: JSON.stringify(response),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Methods': '*',
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
+}
+
+const molecule = {
+  oxygen: '78',
+  hydrogen: '143',
+  carbon: '46',
+  helium: '56',
+  uranium: '7',
+}
+
+const getMolecule = event => {
+  let element = event.pathParameters.element;
+  return molecule[element];
+}
+
 const putMolecule = async event => {
-    let { molecule } = JSON.parse(event.body);
-    let amount = event.pathParameters.amount;
-    let ID = `${molecule}-${amount}`;
-    return Dynamo.increment(ID, 'chemistry-api')
+  let {
+    molecule
+  } = JSON.parse(event.body);
+  let element = event.pathParameters.element;
+  let ID = `${molecule}-${element}`;
+  return Dynamo.increment(ID, 'molecule-element')
 }
 
 // ------------------------------------------------------------------
@@ -18,23 +51,28 @@ const putMolecule = async event => {
 
 // ExpressJS (Jovo Webhook)
 if (process.argv.indexOf('--webhook') > -1) {
-    const port = process.env.PORT || 3000;
-    Webhook.jovoApp = alexa;
+  const port = process.env.PORT || 3000;
+  Webhook.jovoApp = alexa;
 
-    Webhook.listen(port, () => {
-        console.info(`Local server listening on port ${port}.`);
-    });
+  Webhook.listen(port, () => {
+    console.info(`Local server listening on port ${port}.`);
+  });
 
-    Webhook.post('/webhook', async (req, res) => {
-        await alexa.handle(new ExpressJS(req, res));
-    });
+  Webhook.post('/webhook', async (req, res) => {
+    await alexa.handle(new ExpressJS(req, res));
+  });
 }
 
 // AWS Lambda
 exports.handler = async (event, context, callback) => {
-    if (event.httpMethod === 'PUT') {
-        let response = await putMolecule(event)
-        return done(response);
-    }
-    await alexa.handle(new Lambda(event, context, callback));
+  console.log(event);
+  if (event.httpMethod === 'PUT') {
+    let response = await putMolecule(event)
+    return done(response);
+  } else if (event.httpMethod === 'GET') {
+    let response = getMolecule(event);
+    return done(response);
+  }
+
+  await alexa.handle(new Lambda(event, context, callback));
 };
--- server/src/dynamo.js
+++ server/src/dynamo.js
@@ -4,40 +4,20 @@ let documentClient = new AWS.DynamoDB.DocumentClient({
   'region': 'us-east-1'
 });
 
-let params = {
-  TableName: table,
-  Key: {
-    [key]: value
-  }
-};
-
-documentClient.get(params, function (err, data) {
-  if (err) {
-    console.log(`There was an error fetching the data for ${key} ${value} on table ${table}`, err);
-    return reject(err);
-  }
-  return resolve(data.Item);
-});
-
 module.exports = class DB {
-  get(key, value, table) {
-    if (!table) throw 'table needed';
-    if (typeof key !== 'string') throw `key was not string and was ${JSON.stringify(key)} on table ${table}`;
-    if (typeof value !== 'string') throw `value was not string and was ${JSON.stringify(value)} on table ${table}`;
-
-    return new Promise((resolve, reject) => {});
-  }
   write(ID, data, table) {
     return new Promise((resolve, reject) => {
-      if (typeof ID !== 'string') throw `the id must be a string and not ${ID}`;
+      if (typeof ID !== 'string') throw `the id must be a string and not ${ID}`
       if (!data) throw "data is needed";
       if (!table) throw 'table name is needed';
+
       let params = {
         TableName: table,
         Item: { ...data,
           ID: ID
         }
       };
+
       documentClient.put(params, function (err, result) {
         if (err) {
           console.log("Err in writeForCall writing messages to dynamo:", err);
@@ -49,18 +29,41 @@ module.exports = class DB {
           ...params.Item
         });
       });
+    })
+  };
+
+  get(key, value, table) {
+    if (!table) throw 'table needed';
+    if (typeof key !== 'string') throw `key was not string and was ${JSON.stringify(key)} on table ${table}`;
+    if (typeof value !== 'string') throw `value was not string and was ${JSON.stringify(value)} on table ${table}`;
+    return new Promise((resolve, reject) => {
+      let params = {
+        TableName: table,
+        Key: {
+          [key]: value
+        }
+      };
+      documentClient.get(params, function (err, data) {
+        if (err) {
+          console.log(`There was an error fetching the data for ${key} ${value} on table ${table}`, err);
+          return reject(err);
+        }
+        // TODO check only one Item.
+        return resolve(data.Item);
+      });
     });
   }
-  async increment(ID, table, key, value) {
+
+  async increment(ID, element, table) {
     if (!table) throw 'table needed';
     if (!ID) throw 'ID needed';
     let data;
     try {
-      data = await this.get('molecules-amount', ID, table);
-      if (!data.count) throw 'no count in data'
+      data = await this.get('molecules-element', ID, table);
     } catch (err) {
       data = {
-        "molecules-amount": ID,
+        "molecules-element": ID,
+        element,
         count: 0
       };
     };
@@ -68,5 +71,22 @@ module.exports = class DB {
       count: data.count + 1
     };
     return this.write(ID, newData, table);
+  }
+
+  scan(key, value, table) {
+    return new Promise((resolve, reject) => {
+      let params = {
+        TableName: table,
+        FilterExpression: `${key} = :value`,
+        ExpressionAttributeValues: {
+          ':value': value
+        }
+      };
+
+      documentClient.scan(params, function (err, data) {
+        if (err) reject(err)
+        resolve(data);
+      });
+    });
   }
 }
