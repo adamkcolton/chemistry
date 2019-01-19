d9835d84b4370cf2b1f43bdbdc74f4ea57796238
adamkcolton <adamcolton216@gmail.com>
2019-01-18 21:08:33 -0800

    node server is live

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Chemistry
+
+goto the server folder and type in the terminal `yarn install && yarn dev`
---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const SERVER_PORT = 3000;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(express.static('../client'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + 'index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'resource not found',
+  });
+});
+
+http.listen(process.env.PORT || SERVER_PORT, () => {
+  console.log(`Server started on http://localhost:${SERVER_PORT}`);
+});
