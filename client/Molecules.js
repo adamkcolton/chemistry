584d7d89aaf6048cbfdaf6fd0f6a06cdcdb61b86
adamkcolton <adamcolton216@gmail.com>
2019-01-20 04:12:08 -0800

    Adding axios pinging to backend, misc clean up

--- client/Molecules.js
+++ client/Molecules.js
@@ -5,6 +5,7 @@ var reactionId = 0;
 var molecule_to_update = 0;
 var WORLD_SIZE = 5;
 var MAX_SPEED = .04;
+var serverJson;
 
 /* List of molecules on screen*/
 function Molecule(name, posX, posY, posZ) {
@@ -82,30 +83,86 @@ var WATER_POSITIONS = [
 var HELIUM_POSITIONS = [
     "0 0 0"
 ];
-function resetMolecule() {
+
+var num_oxygen = 0;
+var num_glucose = 0;
+var num_uranium = 0;
+var is_cleared = false;
+
+setInterval(function () {
+    axios.get('https://kex84xe01m.execute-api.us-east-1.amazonaws.com/prod/entries').then(function (json) {
+        serverJson = json;
+        console.log(serverJson);
+        for (var entry = 0; entry < serverJson.data.Items.length; entry++) {
+            switch (serverJson.data.Items[entry].element) {
+                case "Oxygen":
+                    if (num_oxygen < serverJson.data.Items[entry].amount) {
+                        var num_new_oxygens = serverJson.data.Items[entry].amount - num_oxygen;
+                        for (var new_items = 0; new_items < num_new_oxygens; new_items++) {
+                            addMolecule('oxygen');
+                        }
+                        num_oxygen = serverJson.data.Items[entry].amount;
+                    }
+                    break;
+                case "Glucose":
+                    if (num_glucose < serverJson.data.Items[entry].amount) {
+                        var num_new_glucoses = serverJson.data.Items[entry].amount - num_glucose;
+                        for (var new_items = 0; new_items < num_new_glucoses; new_items++) {
+                            addMolecule('glucose');
+                        }
+                        num_glucose = serverJson.data.Items[entry].amount;
+                    }
+                    break;
+                case "Uranium":
+                    if (num_uranium < serverJson.data.Items[entry].amount) {
+                        var num_new_uraniums = serverJson.data.Items[entry].amount - num_uranium;
+                        for (var new_items = 0; new_items < num_new_uraniums; new_items++) {
+                            addMolecule('uranium');
+                        }
+                        num_uranium = serverJson.data.Items[entry].amount;
+                    }
+                    break;
+                case "Clear":
+                    if (!is_cleared) {
+                        is_cleared = true;
+                        resetMolecules();
+                    }
+                    break;
+                case "React":
+                    var index = getIdFromMoleculeType('glucose');
+                    if (index == -1) {
+                        index = getIdFromMoleculeType('uranium');
+                    }
+                    reassignElements(index);
+                    break;
+            }
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+}, 1000);
+
+function resetMolecules() {
     moleculeId = 0;
     atomId = 0;
-    for (var i = 0; i < 36; i++) {
+    while (document.getElementById("id" + i) != null) {
         document.getElementById("id" + i).outerHTML = "";
 
         if (i < reactionId) {
             document.getElementById("reaction" + i).outerHTML = "";
         }
+        i++;
     }
     moleculeList = [];
     reactionId = 0;
     molecule_to_update = 0;
 }
 function buttonPress() {
-    if (moleculeList[0].name == 'glucose') {
-        reassignElements(0);
-        setTimeout(function () {
-            resetMolecule();
-        }, 5000);
+    var index = getIdFromMoleculeType('glucose');
+    if (index == -1) {
+        index = getIdFromMoleculeType('uranium');
     }
-    else {
-        setInterval(addReaction, 500);
-    }
+    reassignElements(index);
 }
 
 function addGlucose() {
@@ -115,7 +172,7 @@ function addGlucose() {
 function addOxygen() {
     addMolecule('oxygen');
 }
-function addUranium(){
+function addUranium() {
     addMolecule('uranium');
 }
 
@@ -152,16 +209,17 @@ function addAtom(atom) {
         '<a-sphere id =\"' + "id" + (atomId++) + '\"color = "' + getColorOfAtom(atom) + '" position = "0 ' + getYOfAtom(atom) + ' 0" radius = \"' + getRadiusOfAtom(atom) / 6 + '\"></a-sphere>'
     );
 }
-function addReaction(reactionId) {
+function addReaction(reactionId, combustion_flag) {
     MAX_SPEED = .6;
-    if (moleculeList[0].name == 'glucose') {
+    if (combustion_flag) {
         $("#marker").append(
             '<a-sphere id = "reaction' + reactionId + '\"color = "yellow" position = \"' + (Math.random() * WORLD_SIZE - WORLD_SIZE / 2) + " " + 0 + " " + (Math.random() * WORLD_SIZE - WORLD_SIZE / 2) + '\" radius = ".05"><a-animation attribute = "position" to = \"' + (Math.random() * WORLD_SIZE - WORLD_SIZE / 2) + " " + 0 + " " + (Math.random() * WORLD_SIZE - WORLD_SIZE / 2) + '\" from = "0 0 0" direction = "alternate" dur = "300"></a-animation><a-animation attribute = "scale" direction = "alternate" begin = "100" from = "0 0 0" to = "9 9 9" repeat = "indefinite"></a-animation><a-animation attribute = "visible" dur = "2000" from = "true" to = "false"></a-animation></a-sphere>'
         );
     }
     else {
         addMolecule('helium');
-        document.getElementById("id0").setAttribute("radius", document.getElementById("id0").getAttribute("radius") * .9);
+        var uranium_index = getIdFromMoleculeType('uranium') - 1;
+        document.getElementById("id" + uranium_index).setAttribute("radius", document.getElementById("id" + uranium_index).getAttribute("radius") * .9);
     }
 }
 function getColorOfAtom(atom) {
@@ -216,13 +274,27 @@ setInterval(function () {
     }
 }, 30);
 /* Move molecules around */
-if (moleculeId > 0) {
-    setInterval(function () {
-        console.log(molecule_to_update + " AND " + moleculeId);
+
+setInterval(function () {
+    if (moleculeId > 0) {
         moleculeList[molecule_to_update].posX = Math.random() * WORLD_SIZE - WORLD_SIZE / 2;
         moleculeList[molecule_to_update++].posZ = Math.random() * WORLD_SIZE - WORLD_SIZE / 2;
         if (molecule_to_update >= moleculeId) molecule_to_update = 0;
-    }, 200);
+    }
+}, 2000/moleculeId);
+
+
+function getIdFromMoleculeType(type) {
+    for (var i = 0; i < moleculeId; i++) {
+        if (moleculeList[i].atomList != null) {
+            for (var j = 0; j < moleculeList[i].atomList.length; j++) {
+                if (moleculeList[i].name == type) {
+                    return i;
+                }
+            }
+        }
+    }
+    return -1;
 }
 function getMoleculeFromId(elementNumber) {
     for (var i = 0; i < moleculeId; i++) {
@@ -301,19 +373,19 @@ function reassignElements(moleculeIndex) {
                 moleculeList[moleculeId].atomList.push(brokenMolecule.atomList[6 + water]);
                 moleculeList[moleculeId].atomList.push(brokenMolecule.atomList[(6 + water) * 2]);
                 moleculeList[moleculeId].atomList.push(brokenMolecule.atomList[(6 + water) * 2 + 1]);
-                addReaction(reactionId++);
+                addReaction(reactionId++, true);
                 moleculeId++;
             }
             for (var carbon = 0; carbon < 6; carbon++) {
                 moleculeList.push(new Molecule('carbon_dioxide', Math.random() * WORLD_SIZE - WORLD_SIZE / 2, 0, Math.random() * WORLD_SIZE - WORLD_SIZE / 2));
                 moleculeList[moleculeId].atomList.push(brokenMolecule.atomList[carbon]);
-                addReaction(reactionId++);
+                addReaction(reactionId++, true);
                 /* Find oxygen to combine with */
                 for (var molecule = 0; molecule < moleculeList.length; molecule++) {
                     if (moleculeList[molecule].name == 'oxygen' && moleculeList[molecule].atomList != null) {
                         moleculeList[moleculeId].atomList.push(moleculeList[molecule].atomList[0]);
                         moleculeList[moleculeId].atomList.push(moleculeList[molecule].atomList[1]);
-                        addReaction(reactionId++);
+                        addReaction(reactionId++, true);
                         moleculeList[molecule].atomList = null;
                         break;
                     }
@@ -322,15 +394,10 @@ function reassignElements(moleculeIndex) {
             }
             moleculeList[moleculeIndex].atomList = null;
             break;
-        case ('plutonium'):
+        case ('uranium'):
+            setInterval(addReaction, 500);
             break;
     }
 }
-// axios.get('/api/alexaData/').then(function (json) {
-//     //Build Function gets called or I can have it in here.
-//     console.log(json);
-//     // console.log(json.data);
-//     // buildMolecule(symbol);
-// }).catch(function (error) {
-//     console.log(error);
-// });
+
+
